9a6707eee3644522d6d3c7ce93c8a2affd4252a4
Hung FPT <hungtrong@Hungs-MacBook-Pro.local>
2013-06-18 17:18:48 +0700

    version 0.1

---
+++ HahaProject/HahaProjectTests/HahaProjectTests.m
@@ -0,0 +1,36 @@
+//
+//  HahaProjectTests.m
+//  HahaProjectTests
+//
+//  Created by Hung FPT on 6/18/13.
+//  Copyright (c) 2013 Hung FPT. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HahaProjectTests : XCTestCase
+
+@end
+
+@implementation HahaProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
